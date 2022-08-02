104abd4382462428cbedd9a5f043fb550b720566
KossiviEsse1 <40179699+KossiviEsse1@users.noreply.github.com>
2022-08-01 20:15:47 -0500

    Added Base Files For Our Project

---
+++ VolunteerOrganizer/src/main/java/org/launchcode/VolunteerOrganizer/VolunteerOrganizerApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.VolunteerOrganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteerOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolunteerOrganizerApplication.class, args);
+	}
+
+}
