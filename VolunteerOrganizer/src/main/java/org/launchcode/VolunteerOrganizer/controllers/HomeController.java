36a3316e3fc54fca2071f8df35eba980105ca70e
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-08-10 12:40:46 -0600

    Created GetMapping method in HomeController to display index page.

--- VolunteerOrganizer/src/main/java/org/launchcode/VolunteerOrganizer/controllers/HomeController.java
+++ VolunteerOrganizer/src/main/java/org/launchcode/VolunteerOrganizer/controllers/HomeController.java
@@ -1,4 +1,13 @@
 package org.launchcode.VolunteerOrganizer.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping()
+    public String index() {
+        return "index";
+    }
 }
