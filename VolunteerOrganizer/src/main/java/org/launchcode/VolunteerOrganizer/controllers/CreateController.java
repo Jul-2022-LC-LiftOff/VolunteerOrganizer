82a14f824a1485784fbd7d228e321acc355b13b5
AnjanaSujith <99908479+AnjanaSujith@users.noreply.github.com>
2022-08-11 16:45:10 -0500

    Create update (#13)
    * Chnaged create .html
    
    * Chaged org description and age group, orgname changed to dropdown in create .html. List added to controller to hold orgname

--- VolunteerOrganizer/src/main/java/org/launchcode/VolunteerOrganizer/controllers/CreateController.java
+++ VolunteerOrganizer/src/main/java/org/launchcode/VolunteerOrganizer/controllers/CreateController.java
@@ -1,14 +1,22 @@
 package org.launchcode.VolunteerOrganizer.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("create")
 public class CreateController {
+    private static List<String> orgnames = new ArrayList<>();
     @GetMapping
-    public String renderCreateOpportunityForm(){
+    public String renderCreateOpportunityForm(Model model){
+        orgnames.add("YMCA");
+        orgnames.add("Police Department");
+        model.addAttribute("orgnames",orgnames);
         return "create";
     }
 }
