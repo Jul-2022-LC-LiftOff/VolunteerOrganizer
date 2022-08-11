28db4d8aee25be53980fd2bf718191ba4835316f
Christopher Clay Jr <92304659+3Topher@users.noreply.github.com>
2022-08-10 19:46:38 -0500

    added user model (#12)
    User model, controller, and template look good

---
+++ VolunteerOrganizer/src/main/java/org/launchcode/VolunteerOrganizer/models/User.java
@@ -0,0 +1,24 @@
+package org.launchcode.VolunteerOrganizer.models;
+
+public class User {
+
+    private String username;
+    private String pwHash;
+    private String accountType;
+
+    public User() {}
+
+    public User(String username, String password, String accountType) {
+        this.username = username;
+        this.pwHash = password;
+        this.accountType = accountType;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+}
