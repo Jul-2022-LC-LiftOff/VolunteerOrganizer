400802ce5cf27b9ed2e0742134480a79629bf570
unknown <christopherclay4@gmail.com>
2022-08-10 19:41:00 -0500

    added user model

---
+++ VolunteerOrganizer/src/main/java/org/launchcode/VolunteerOrganizer/controllers/LoginController.java
@@ -0,0 +1,23 @@
+package org.launchcode.VolunteerOrganizer.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("login")
+    public String displayLoginForm() {
+        return "loginNew";
+    }
+
+    @PostMapping("login")
+    @ResponseBody
+    public String displayFormData(@RequestParam String username, String password) {
+
+        return "string";
+    }
+}
