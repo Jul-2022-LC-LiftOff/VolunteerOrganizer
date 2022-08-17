736e9d6f12e2dfb3a53883ca75d02130cc9395f2
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-08-17 12:01:48 -0500

    LoginController Get method now returns correct 'login' template

--- VolunteerOrganizer/src/main/java/org/launchcode/VolunteerOrganizer/controllers/LoginController.java
+++ VolunteerOrganizer/src/main/java/org/launchcode/VolunteerOrganizer/controllers/LoginController.java
@@ -11,7 +11,7 @@ public class LoginController {
 
     @GetMapping("login")
     public String displayLoginForm() {
-        return "loginNew";
+        return "login";
     }
 
     @PostMapping("login")
