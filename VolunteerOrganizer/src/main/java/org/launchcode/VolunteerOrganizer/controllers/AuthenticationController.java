ea68de51d2bc77c25dc10e7cd7d0c741e50ba8c6
Philip Abbyad <96091530+philipabbyad@users.noreply.github.com>
2022-08-17 19:03:37 -0600

    Process login form (#21)
    * Added dependency for bycrypt hash algorithm.
    
    * Added password hashing and comparison method to User model.
    
    * Created User Repository.
    
    * Added annotations to User model.
    
    * Created Authentication Controller for handling of login and registration forms.
    
    * Added session-handling utilities.
    
    * Modified AuthenticationController as User model does not yet include .getId() method.
    
    * Added login form DTO. Password currently requires minimum 6 characters.
    
    * Modified Authentication Controller to handle login template.
    
    * Added Id field and getter to User model. Updated AuthenticationController to reflect this.
    
    * Removed login controller, now part of authentication controller.
    
    * Added example user to test login feature.
    
    * Resolved merge conflict
    
    * Removed temporary login-new template.
    
    * Removed example user for login test.
    
    * Created session key for user account type

---
+++ VolunteerOrganizer/src/main/java/org/launchcode/VolunteerOrganizer/controllers/AuthenticationController.java
@@ -0,0 +1,93 @@
+package org.launchcode.VolunteerOrganizer.controllers;
+
+import org.launchcode.VolunteerOrganizer.models.User;
+import org.launchcode.VolunteerOrganizer.models.data.UserRepository;
+import org.launchcode.VolunteerOrganizer.models.dto.LoginFormDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import java.util.Optional;
+
+@Controller
+public class AuthenticationController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    private static final String userSessionKey = "userId";
+    private static final String userAccountType = "accountType";
+
+    public User getUserFromSession(HttpSession session) {
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        if (userId == null) {
+            return null;
+        }
+
+        Optional<User> user = userRepository.findById(userId);
+
+        if (user.isEmpty()) {
+            return null;
+        }
+
+        return user.get();
+    }
+
+
+    private static void setUserInSession(HttpSession session, User user) {
+        session.setAttribute(userSessionKey, user.getId());
+        session.setAttribute(userAccountType, user.getAccountType());
+    }
+
+    @GetMapping("/login")
+    public String displayLoginForm(Model model) {
+        model.addAttribute(new LoginFormDTO());
+        model.addAttribute("title", "Log In");
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String processLoginForm(@ModelAttribute @Valid LoginFormDTO loginFormDTO,
+                                   Errors errors, HttpServletRequest request,
+                                   Model model) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Log In");
+            return "login";
+        }
+
+        User theUser = userRepository.findByUsername(loginFormDTO.getUsername());
+
+        if (theUser == null) {
+            errors.rejectValue("username", "user.invalid", "The given username does not exist");
+            model.addAttribute("title", "Log In");
+            return "login";
+        }
+
+        String password = loginFormDTO.getPassword();
+
+        if (!theUser.isMatchingPassword(password)) {
+            errors.rejectValue("password", "password.invalid", "Invalid password");
+            model.addAttribute("title", "Log In");
+            return "login";
+        }
+
+        setUserInSession(request.getSession(), theUser);
+
+        return "redirect:";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request){
+        request.getSession().invalidate();
+        return "redirect:/login";
+    }
+}
+
--- VolunteerOrganizer/src/main/java/org/launchcode/VolunteerOrganizer/models/User.java
+++ VolunteerOrganizer/src/main/java/org/launchcode/VolunteerOrganizer/models/User.java
@@ -1,17 +1,35 @@
 package org.launchcode.VolunteerOrganizer.models;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
     private String username;
+    @NotNull
     private String pwHash;
     private String accountType;
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
     public User() {}
 
     public User(String username, String password, String accountType) {
         this.username = username;
-        this.pwHash = password;
+        this.pwHash = encoder.encode(password);
         this.accountType = accountType;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getUsername() {
@@ -20,5 +38,9 @@ public class User {
 
     public String getAccountType() {
         return accountType;
+    }
+
+    public boolean isMatchingPassword(String password) {
+        return encoder.matches(password, pwHash);
     }
 }
