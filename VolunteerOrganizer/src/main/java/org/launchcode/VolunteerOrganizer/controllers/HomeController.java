b4fe0032d8c9ac7f6d9ef880a87edd869ee97c29
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-08-20 09:54:02 -0500

    Added controller for home page.

---
+++ VolunteerOrganizer/src/main/java/org/launchcode/VolunteerOrganizer/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package org.launchcode.VolunteerOrganizer.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String displayHome() {
+        return "home";
+    }
+}
