cc88e25ea0858a52daff4b20bb247486719ecfda
maks075 <shlapak.maksym@gmail.com>
2022-08-21 18:27:50 -0500

    navigation bar done

--- VolunteerOrganizer/src/main/java/org/launchcode/VolunteerOrganizer/controllers/HomeController.java
+++ VolunteerOrganizer/src/main/java/org/launchcode/VolunteerOrganizer/controllers/HomeController.java
@@ -1,12 +1,15 @@
 package org.launchcode.VolunteerOrganizer.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/home")
-    public String displayHome() {
+    public String displayHome(Model model) {
+        model.addAttribute("title", "Home");
+
         return "home";
     }
 }
