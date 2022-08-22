b8168409dc3487546165a8666069809fcccad5a0
Philip Abbyad <96091530+philipabbyad@users.noreply.github.com>
2022-08-22 11:46:29 -0600

    Home view update (#26)
    * Updated header navigation bar. Updating Home view.
    
    * Moved search functionality to Home view. Removed previous search controller and template files.
    
    * Updated Create Controller to return user to Home page on successful form submission.

--- VolunteerOrganizer/src/main/java/org/launchcode/VolunteerOrganizer/controllers/HomeController.java
+++ VolunteerOrganizer/src/main/java/org/launchcode/VolunteerOrganizer/controllers/HomeController.java
@@ -1,15 +1,44 @@
 package org.launchcode.VolunteerOrganizer.controllers;
 
+import org.launchcode.VolunteerOrganizer.models.Opportunity;
+import org.launchcode.VolunteerOrganizer.models.OpportunityData;
+import org.launchcode.VolunteerOrganizer.models.data.OpportunityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.text.ParseException;
+
+@RequestMapping("home")
 @Controller
 public class HomeController {
-    @GetMapping("/home")
+
+    @Autowired
+    private OpportunityRepository opportunityRepository;
+
+    @GetMapping("")
     public String displayHome(Model model) {
         model.addAttribute("title", "Home");
+        return "home";
+    }
+
+    @PostMapping("/results")
+    public String displaySearchResults(Model model, @RequestParam String searchTerm, @RequestParam String category, @RequestParam String start, @RequestParam String end) throws ParseException {
+        Iterable<Opportunity> opportunities;
+
+        opportunities = OpportunityData.findBySearchTerm(searchTerm, opportunityRepository.findAll());
+        opportunities = OpportunityData.findByCategory(category, opportunities);
+        opportunities = OpportunityData.findByDate(start, end, opportunities);
+
+        model.addAttribute("title", "Home");
+        model.addAttribute("resultsTitle", "Search results:");
+        model.addAttribute("opportunities", opportunities);
 
         return "home";
     }
+
 }
