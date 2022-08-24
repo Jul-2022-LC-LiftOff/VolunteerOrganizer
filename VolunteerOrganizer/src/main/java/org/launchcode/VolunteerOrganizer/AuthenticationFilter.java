10a7e59ab8b804a97e9a5a36da488ba469f4349b
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-08-23 19:21:46 -0600

    Added preHandle method.

--- VolunteerOrganizer/src/main/java/org/launchcode/VolunteerOrganizer/AuthenticationFilter.java
+++ VolunteerOrganizer/src/main/java/org/launchcode/VolunteerOrganizer/AuthenticationFilter.java
@@ -1,4 +1,39 @@
 package org.launchcode.VolunteerOrganizer;
 
-public class AuthenticationFilter {
+import org.launchcode.VolunteerOrganizer.controllers.AuthenticationController;
+import org.launchcode.VolunteerOrganizer.models.User;
+import org.launchcode.VolunteerOrganizer.models.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class AuthenticationFilter implements HandlerInterceptor {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    AuthenticationController authenticationController;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+                            throws IOException {
+
+        HttpSession session = request.getSession();
+        User user = authenticationController.getUserFromSession(session);
+
+        if (user != null) {
+            return true;
+        } else {
+            response.sendRedirect("/");
+            return false;
+        }
+
+    }
+
+
 }
